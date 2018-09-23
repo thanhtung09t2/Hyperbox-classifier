Fix bug draw hyperbox for high dimensional datasets + Ignore sample outside normalization range

EFMN/efmnnclassification.py
@@ -199,9 +199,9 @@ class EFMNNClassification(BaseFMNNClassifier):
                         if self.classId[-1] < len(mark_col):
                             box_color = mark_col[self.classId[-1]]
                             
-                            hyperbox = drawbox(np.asmatrix(Xh[i, 0:np.minimum(xX, 3)]), np.asmatrix(Xh[i, 0:np.minimum(xX, 3)]), drawing_canvas, box_color)
-                            listLines.append(hyperbox[0])
-                            self.delay()
+                        hyperbox = drawbox(np.asmatrix(Xh[i, 0:np.minimum(xX, 3)]), np.asmatrix(Xh[i, 0:np.minimum(xX, 3)]), drawing_canvas, box_color)
+                        listLines.append(hyperbox[0])
+                        self.delay()
                             
            						
         return self

EFMN/knefmnnclassification.py
@@ -83,7 +83,7 @@ class KNEFMNNClassification(BaseFMNNClassifier):
                 if xX == 2:
                     drawing_canvas.plot(Xh[i, 0], Xh[i, 1], color = color_, marker=marker_)
                 else:
-                    drawing_canvas.plot([Xh[i, 0]], [Xh[i, 1]], [Xh[i, 2]], color = color_, marker=marker_)
+                    drawing_canvas.plot([Xh[i, 0]], [Xh[i, 1]], [Xh[i, 2]])#, color = color_, marker=marker_)
                 
                 self.delay()
                 
@@ -114,13 +114,17 @@ class KNEFMNNClassification(BaseFMNNClassifier):
                     
                     indexSort = np.argsort(b)[::-1]
                     
-                    K = np.minimum(K, len(indexSort))
+                    if K > len(indexSort):
+                        K = len(indexSort)
                     
                     isHaveWinner = False
                     
                     for kk in range(K):
                         # store the index of the winner hyperbox in the list of all hyperboxes of all classes
                         j = idSameClassOfX[indexSort[kk]]
+                        
+#                        if kk > 0:
+#                            print("Choose the next hyperbox")
                     
                         if b[indexSort[kk]] != 1:                   
                             # test violation of max hyperbox size and class labels
@@ -140,10 +144,12 @@ class KNEFMNNClassification(BaseFMNNClassifier):
                                     try:
                                         listLines[j].remove()
                                     except:
+                                        print("Error remove box")
                                         pass
                                     
-                                    hyperbox = drawbox(np.asmatrix(self.V[j, 0:np.minimum(xX, 3)]), np.asmatrix(self.W[j, 0:np.minimum(xX, 3)]), drawing_canvas, box_color)                                 
+                                    hyperbox = drawbox(np.asmatrix(self.V[j, 0:np.minimum(xX, 3)]), np.asmatrix(self.W[j, 0:np.minimum(xX, 3)]), drawing_canvas, box_color)
                                     listLines[j] = hyperbox[0]
+                                       
                                     self.delay()
                                 
                                 if self.V.shape[0] > 1:
@@ -210,9 +216,9 @@ class KNEFMNNClassification(BaseFMNNClassifier):
                         if self.classId[-1] < len(mark_col):
                             box_color = mark_col[self.classId[-1]]
                             
-                            hyperbox = drawbox(np.asmatrix(Xh[i, 0:np.minimum(xX, 3)]), np.asmatrix(Xh[i, 0:np.minimum(xX, 3)]), drawing_canvas, box_color)
-                            listLines.append(hyperbox[0])
-                            self.delay()
+                        hyperbox = drawbox(np.asmatrix(Xh[i, 0:np.minimum(xX, 3)]), np.asmatrix(Xh[i, 0:np.minimum(xX, 3)]), drawing_canvas, box_color)
+                        listLines.append(hyperbox[0])
+                        self.delay()
                             
            						
         return self

FMNN/fmnnclassification.py
@@ -138,8 +138,8 @@ class FMNNClassification(BaseFMNNClassifier):
                                     listLines[j].remove()
                                 except:
                                     pass
-                                
-                                hyperbox = drawbox(np.asmatrix(self.V[j, 0:np.minimum(xX, 3)]), np.asmatrix(self.W[j, 0:np.minimum(xX, 3)]), drawing_canvas, box_color)                                 
+                                    
+                                hyperbox = drawbox(np.asmatrix(self.V[j, 0:np.minimum(xX, 3)]), np.asmatrix(self.W[j, 0:np.minimum(xX, 3)]), drawing_canvas, box_color)
                                 listLines[j] = hyperbox[0]
                                 self.delay()
                                     
@@ -199,9 +199,9 @@ class FMNNClassification(BaseFMNNClassifier):
                         if self.classId[-1] < len(mark_col):
                             box_color = mark_col[self.classId[-1]]
                             
-                            hyperbox = drawbox(np.asmatrix(Xh[i, 0:np.minimum(xX, 3)]), np.asmatrix(Xh[i, 0:np.minimum(xX, 3)]), drawing_canvas, box_color)
-                            listLines.append(hyperbox[0])
-                            self.delay()
+                        hyperbox = drawbox(np.asmatrix(Xh[i, 0:np.minimum(xX, 3)]), np.asmatrix(Xh[i, 0:np.minimum(xX, 3)]), drawing_canvas, box_color)
+                        listLines.append(hyperbox[0])
+                        self.delay()
                             
            						
         return self
@@ -266,7 +266,7 @@ if __name__ == '__main__':
         
     classifier = FMNNClassification(gamma, teta, isDraw, isNorm, norm_range)
     classifier.fit(Xtr, patClassIdTr)
-    
+    print('Final hyperbox No =', len(classifier.classId))
     # Testing
     print("-- Testing --")
     result = classifier.predict(Xtest, patClassIdTest)

GFMM/basegfmmclassifier.py
@@ -171,6 +171,7 @@ class BaseGFMMClassifier(object):
                           + out              Soft class memberships
                           + mem              Hyperbox memberships
         """
+        Xl_Test, Xu_Test = delete_const_dims(Xl_Test, Xu_Test)
         # Normalize testing dataset if training datasets were normalized
         if len(self.mins) > 0:
             noSamples = Xl_Test.shape[0]
@@ -178,8 +179,19 @@ class BaseGFMMClassifier(object):
             Xu_Test = self.loLim + (self.hiLim - self.loLim) * (Xu_Test - np.ones((noSamples, 1)) * self.mins) / (np.ones((noSamples, 1)) * (self.maxs - self.mins))
             
             if Xl_Test.min() < self.loLim or Xu_Test.min() < self.loLim or Xl_Test.max() > self.hiLim or Xu_Test.max() > self.hiLim:
-                print('Test sample falls ousitde', self.loLim, '-', self.hiLim, 'interval')
-                return
+                print('Test sample falls outside', self.loLim, '-', self.hiLim, 'interval')
+                print('Number of original samples = ', noSamples)
+                
+                # only keep samples within the interval loLim-hiLim
+                indXl_good = np.where((Xl_Test >= self.loLim).all(axis = 1) & (Xl_Test <= self.hiLim).all(axis = 1))[0]
+                indXu_good = np.where((Xu_Test >= self.loLim).all(axis = 1) & (Xu_Test <= self.hiLim).all(axis = 1))[0]
+                indKeep = np.intersect1d(indXl_good, indXu_good)
+                
+                Xl_Test = Xl_Test[indKeep, :]
+                Xu_Test = Xu_Test[indKeep, :]
+                
+                print('Number of kept samples =', Xl_Test.shape[0])
+                #return
             
         # do classification
         result = predict(self.V, self.W, self.classId, Xl_Test, Xu_Test, patClassIdTest, self.gamma, self.oper)